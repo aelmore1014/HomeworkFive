2916a221c8ea257cf9ef18eeb0b99760ee6ee06e
Alex Elmore <alexelmore1014@gmail.com>
2019-11-19 14:04:51 -0500

    added the layout and javascript for saving entries

--- script.js
+++ script.js
@@ -7,3 +7,89 @@ var twoPM = $("#2pmInput");
 var threePM = $("#3pmInput");
 var fourPM = $("#4pmInput");
 var fivePM = $("#5pmInput");
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#9amInput").val();
+    localStorage.setItem("9am", userEvent);
+});
+function DisplayEvent9() {
+    $("#9amInput").val(localStorage.getItem("9am"))
+}
+DisplayEvent9();
+
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#10amInput").val();
+    localStorage.setItem("10am", userEvent);
+});
+function DisplayEvent10() {
+    $("#10amInput").val(localStorage.getItem("10am"))
+}
+DisplayEvent10();
+
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#11amInput").val();
+    localStorage.setItem("11am", userEvent);
+});
+function DisplayEvent11() {
+    $("#11amInput").val(localStorage.getItem("11am"))
+}
+DisplayEvent11();
+
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#12pmInput").val();
+    localStorage.setItem("12pm", userEvent);
+});
+function DisplayEvent12() {
+    $("#12pmInput").val(localStorage.getItem("12pm"))
+}
+DisplayEvent12();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#1pmInput").val();
+    localStorage.setItem("1pm", userEvent);
+});
+function DisplayEvent1() {
+    $("#1pmInput").val(localStorage.getItem("1pm"))
+}
+DisplayEvent1();
+
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#2pmInput").val();
+    localStorage.setItem("2pm", userEvent);
+});
+function DisplayEvent2() {
+    $("#2pmInput").val(localStorage.getItem("2pm"))
+}
+DisplayEvent2();
+
+
+$(".saveBtn").click(function () {
+    var userEvent = $("3pmInput").val();
+    localStorage.setItem("3pm", userEvent);
+});
+function DisplayEvent3() {
+    $("#3pmInput").val(localStorage.getItem("3pm"))
+}
+DisplayEvent3();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("4pmInput").val();
+    localStorage.setItem("4pm", userEvent);
+});
+function DisplayEvent4() {
+    $("#4pmInput").val(localStorage.getItem("4pm"))
+}
+DisplayEvent4();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("5pmInput").val();
+    localStorage.setItem("5pm", userEvent);
+});
+function DisplayEvent4() {
+    $("#5pmInput").val(localStorage.getItem("5pm"))
+}
+DisplayEvent5();
