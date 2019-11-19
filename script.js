4d8c2d4c0fa1b8db381a9759443d337cd1e98c22
Alex Elmore <alexelmore1014@gmail.com>
2019-11-19 17:06:03 -0500

    added screenshots

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ DisplayEvent2();
 
 
 $(".saveBtn").click(function () {
-    var userEvent = $("3pmInput").val();
+    var userEvent = $("#3pmInput").val();
     localStorage.setItem("3pm", userEvent);
 });
 function DisplayEvent3() {
@@ -77,7 +77,7 @@ function DisplayEvent3() {
 DisplayEvent3();
 
 $(".saveBtn").click(function () {
-    var userEvent = $("4pmInput").val();
+    var userEvent = $("#4pmInput").val();
     localStorage.setItem("4pm", userEvent);
 });
 function DisplayEvent4() {
@@ -86,10 +86,101 @@ function DisplayEvent4() {
 DisplayEvent4();
 
 $(".saveBtn").click(function () {
-    var userEvent = $("5pmInput").val();
+    var userEvent = $("#5pmInput").val();
     localStorage.setItem("5pm", userEvent);
 });
-function DisplayEvent4() {
+function DisplayEvent5() {
     $("#5pmInput").val(localStorage.getItem("5pm"))
 }
 DisplayEvent5();
+
+var dateEl = $("#todaysDate");
+var date = new Date();
+date= moment().format('LL');
+var theDate = moment().format("h A");
+dateEl.text(date);
+console.log(theDate);
+
+if (theDate === "9 AM"){
+    NineAm.addClass("present")
+}
+
+
+if (theDate === "10 AM"){
+    tenAM.addClass("present")
+}
+
+else {
+    tenAM.addClass("future")
+}
+
+if (theDate === "11 AM"){
+    elevnAM.addClass("present")
+}
+
+else {
+    elevenAM.addClass("future")
+}
+
+if (theDate=== "12 PM"){
+    twelvePM.addClass("present")
+}
+
+else {
+    twelvePM.addClass("future")
+}
+
+if (theDate === "1 PM"){
+    onePM.addClass("present")
+}
+
+else {
+    onePM.addClass("future")
+}
+
+
+if (theDate === "2 PM"){
+    twoPM.addClass("present")
+}
+
+else {
+    twoPM.addClass("future")
+}
+
+if (theDate === "3 PM"){
+    threePM.addClass("present")
+}
+
+else {
+    threePM.addClass("future")
+}
+
+if (theDate === "4 PM"){
+    fourPM.addClass("present")
+}
+
+else {
+    fourPM.addClass("future")
+}
+
+if (theDate=== "5 PM"){
+    fivePM.addClass("present")
+}
+
+else {
+    fivePM.addClass("future")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
